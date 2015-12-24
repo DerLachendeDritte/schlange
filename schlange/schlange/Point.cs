6234216dfe8e4b4ed2696cc90e19e4ec3d47eeaf
Stirlitz <sashok-pisklov@yandex.ru>
2015-12-24 17:51:38 +0200

    Point class constructors written

--- schlange/schlange/Point.cs
+++ schlange/schlange/Point.cs
@@ -12,6 +12,14 @@ namespace schlange
         public int y;
         public char sym;
 
+
+        //создаем конструктор
+        public Point(int _x, int _y, char _sym)
+        {
+            x = _x;
+            y = _y;
+            sym = _sym;
+        }
         public void Draw()
         {
             Console.SetCursorPosition(this.x, this.y);
